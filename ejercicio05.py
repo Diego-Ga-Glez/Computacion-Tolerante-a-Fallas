82361eb1e40561c65138d179afbd329727c3b439
Diego Garcia Gonzalez <diego.garcia2084@alumnos.udg.mx>
2022-02-03 18:37:07 -0600

    Manejo de errores en python (Actividad 05)

---
+++ ejercicio05.py
@@ -0,0 +1,20 @@
+while True:
+    try:
+        var = int(input("Digite un numero entre 0 y 10: "))
+
+        if var < 0:
+            assert False
+        elif var > 10:
+            raise Exception
+        
+    except ValueError:
+        print("\nLa entrada no es un numero :(")
+    except AssertionError:
+        print("\nNumero menor a 0")
+    except Exception:
+        print("\nNumero mayor a 10")
+
+    else:
+        print("\nLa entrada es un numero entre 0 y 10")
+    finally:
+        print("\nFin del script\n")
