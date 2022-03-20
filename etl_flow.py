9927ac509fe10f7a62adc29ea9c6bee42f7659d2
Diego Garcia Gonzalez <diego.garcia2084@alumnos.udg.mx>
2022-03-19 21:57:41 -0600

    contenedor de aplicacion en python

--- etl_flow.py
+++ etl_flow.py
@@ -10,26 +10,15 @@ def extract(url):
 
 @task
 def transform(datos):
-    dic = {}
     lista = []
 
-    if(len(datos) != 4):
-        for i in datos:
-            dic = {
-                "ID_usuario" : i["userId"],
-                "ID" : i["id"],
-                "titulo" : i["title"],
-                "completado" : i["completed"] 
-            }
-            lista.append(dic)
-    else:
-        dic = {
-                "ID_usuario" : datos["userId"],
-                "ID" : datos["id"],
-                "titulo" : datos["title"],
-                "completado" : datos["completed"] 
-            }
-        lista.append(dic)
+    for i in datos:
+        lista.append({
+            "ID_usuario" : i["userId"],
+            "ID" : i["id"],
+            "titulo" : i["title"],
+            "completado" : i["completed"] 
+            })
 
     return lista
 
@@ -38,28 +27,18 @@ def load(datos,path):
     with open(path, "w") as f:
         json.dump(datos,f, indent=4)
 
-def etl_flow(dato):
+def etl_flow():
     with Flow("etl_flow") as flow:
-        e = extract("https://jsonplaceholder.cypress.io/todos" + dato)
+        e = extract("https://jsonplaceholder.cypress.io/todos")
         t = transform(e)
         load(t,"datos.json")
 
     return flow
 
-while True:
-    print()
-    print("1. Extraer toda la informacion")
-    print("2. Extraer un dato")
-    opc = int(input(">>"))
-    print()
 
-    if opc == 1:
-        flow = etl_flow("")
-        flow.run()
-    if opc == 2:
-        aux = int(input("Ingrese un numero: "))  
-        flow = etl_flow("/"+ str(aux))
-        flow.run()
+flow = etl_flow()
+flow.run()
+    
 
 
 
